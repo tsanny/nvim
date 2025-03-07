f0c5d86e82b1af6ea0e223fa893d3cf280fadcd6
tsanny <sulthanftr@gmail.com>
2025-03-07 18:07:55 +0700

    update

--- lua/theprimeagen/init.lua
+++ lua/theprimeagen/init.lua
@@ -49,11 +49,8 @@ autocmd({"BufWritePre"}, {
 autocmd('BufEnter', {
     group = ThePrimeagenGroup,
     callback = function()
-        if vim.bo.filetype == "zig" then
-            vim.cmd.colorscheme("tokyonight-night")
-        else
-            vim.cmd.colorscheme("rose-pine-moon")
-        end
+        vim.cmd.colorscheme("tokyonight-night")
+        -- vim.cmd.colorscheme("rose-pine-moon")
     end
 })
 
