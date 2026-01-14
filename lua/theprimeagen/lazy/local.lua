859b1a72455187f37a264b5daf3e4ecc1a97b99a
sf.tsany <sf.tsany@shirokuma069.local>
2026-01-14 09:54:48 +0900

    update

--- lua/theprimeagen/lazy/local.lua
+++ lua/theprimeagen/lazy/local.lua
@@ -1,32 +1,27 @@
 
 local local_plugins = {
-    -- {
-    --     "caleb",
-    --     dir = "~/personal/caleb",
-    --     config = function() end
-    -- },
-    -- {
-    --     "harpoon",
-    --     dir = "~/personal/harpoon",
-    --     config = function()
-    --         local harpoon = require("harpoon")
-    --
-    --         harpoon:setup()
-    --
-    --         vim.keymap.set("n", "<leader>A", function() harpoon:list():prepend() end)
-    --         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-    --         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-    --
-    --         vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-    --         vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-    --         vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-    --         vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-    --         vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
-    --         vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
-    --         vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
-    --         vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
-    --     end
-    -- },
+    {
+        "ThePrimeagen/harpoon",
+        config = function()
+            local mark = require("harpoon.mark")
+            local harpoon_ui = require("harpoon.ui")
+
+            require("harpoon").setup()
+
+            vim.keymap.set("n", "<leader>A", function() mark.add_file() end)
+            vim.keymap.set("n", "<leader>a", function() mark.add_file() end)
+            vim.keymap.set("n", "<C-e>", harpoon_ui.toggle_quick_menu)
+
+            vim.keymap.set("n", "<C-h>", function() harpoon_ui.nav_file(1) end)
+            vim.keymap.set("n", "<C-j>", function() harpoon_ui.nav_file(2) end)
+            vim.keymap.set("n", "<C-k>", function() harpoon_ui.nav_file(3) end)
+            vim.keymap.set("n", "<C-l>", function() harpoon_ui.nav_file(4) end)
+            vim.keymap.set("n", "<leader><C-h>", function() mark.rm_file(1) end)
+            vim.keymap.set("n", "<leader><C-j>", function() mark.rm_file(2) end)
+            vim.keymap.set("n", "<leader><C-k>", function() mark.rm_file(3) end)
+            vim.keymap.set("n", "<leader><C-l>", function() mark.rm_file(4) end)
+        end
+    },
     -- {
     --     "vim-apm", dir = "~/personal/vim-apm",
     --     config = function()
@@ -45,4 +40,3 @@ local local_plugins = {
 }
 
 return local_plugins
-
