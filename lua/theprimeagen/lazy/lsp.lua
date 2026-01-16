c5f176a21a59633ed78931af353fc2d76ab338f5
sf.tsany <sf.tsany@shirokuma069.local>
2026-01-16 10:30:34 +0900

    update

--- lua/theprimeagen/lazy/lsp.lua
+++ lua/theprimeagen/lazy/lsp.lua
@@ -18,7 +18,8 @@ return {
     config = function()
         require("conform").setup({
             formatters_by_ft = {
-            }
+            },
+            format_on_save = false, -- keep formatting manual rather than running on every write
         })
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
